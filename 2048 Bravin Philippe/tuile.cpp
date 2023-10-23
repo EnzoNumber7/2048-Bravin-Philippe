d739132bfae4a5c593bfa4b23cf780b038ca0510
EnzoNumber7 <117282665+EnzoNumber7@users.noreply.github.com>
2023-10-23 18:05:36 +0200

    omg ca marche

---
+++ 2048 Bravin Philippe/tuile.cpp
@@ -0,0 +1,6 @@
+#include "tuile.h"
+
+Tuile::Tuile() {
+	t_pos_X = -1;
+	t_pos_Y = -1;
+}
---
+++ 2048 Bravin Philippe/tuile.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Tuile
+{
+private:
+	int t_value;
+	int t_pos_X;
+	int t_pos_Y;
+
+public:
+
+	Tuile Tuile();
+};
